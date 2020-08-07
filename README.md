ae9e80386991fb53cd5cafdba247ca22d75347fc
mashile <sakkiemashile@gmail.com>
2020-08-07 14:25:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-j1j8z5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-j1j8z5)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './bootstrap.css';
+import './tooplate-style.css';
+import './bootstrap-theme.css';
+import './datepicker.css';
+import './fontAwesome.css';
+import './hero-slider.css';
+import './owl-carousel.css';
+import './hero-slider.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>js starter</h1>`;

3f37ad272693288d1ae27c9d132bbd5682795f3f
mashile <sakkiemashile@gmail.com>
2020-08-09 14:29:38 +0200

    4 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ import './fontAwesome.css';
 import './hero-slider.css';
 import './owl-carousel.css';
 import './hero-slider.css';
+//import React, { Component } from 'react';
+//import { render } from 'react-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
+//import Hello from './Hello';
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: 'React'
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Hello name={this.state.name} />
+//         <p>
+//           Start editing to see some magic happen :)
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+//const element = <FontAwesomeIcon icon={faBars} size={"2x"}/>
+
+//render(element, document.getElementById('root'));
+
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>js starter</h1>`;
+//const element = <fontAwesomeIcon icon={fabars} size={"2x"}/>
+
+//render(element, document.getElementById('root'));
